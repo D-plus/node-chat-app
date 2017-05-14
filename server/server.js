c9f9fbdc5b9a9db023d9e142b97631ead357e15d
Dmitriy <Dmitriy@MacBook-Pro.local>
2017-05-14 14:15:09 +0300

    added form form typing messages from browser and ability to show those in browser

--- server/server.js
+++ server/server.js
@@ -9,35 +9,26 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+const { generateMessage } = require('./utils/message');
+
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+// io.emit send event to all users,
+// socket.emit emit event only to current user,
+// socket.broadcast.emit emit event to all users excluding current user
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+
+  socket.on('createMessage', (message, callback) => {
+    //console.log('createMessage', message);
+
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback ? callback({ status: 'sent successfuly'}) : null;
   });
 
   socket.on('disconnect', () => {
